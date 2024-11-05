79284ee4df6b0c4d144e28f29f2ed78b79811eea
卢文武 <wu6789@qq.com>
2024-11-05 15:15:23 +0800

    添加ntp服务时间同步脚本

---
+++ bat/设置ntp服务并同步时间.bat
@@ -0,0 +1,10 @@
+@echo off
+
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" /v NtpServer /d "192.168.0.103" /f
+
+net stop w32time
+net start w32time
+
+w32tm /resync
+
+pause
