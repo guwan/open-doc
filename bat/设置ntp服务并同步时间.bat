80b8c202da429b2b6420b95022361fae142b60f9
卢文武 <wu6789@qq.com>
2024-11-21 16:17:15 +0800

    将windows时间服务先设置为自动启动。

--- bat/设置ntp服务并同步时间.bat
+++ bat/设置ntp服务并同步时间.bat
@@ -1,5 +1,7 @@
 @echo off
 
+sc config w32time start= auto
+
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" /v NtpServer /d "192.168.0.103" /f
 
 net stop w32time
